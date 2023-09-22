4e38a04cb7050580d508b1c67abcf9edc669d49d
Hamed Haghjo <hamedhaghjo@hotmail.com>
2023-09-22 04:08:53 +0200

    Add description and classifiers

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setup(
     },
     author="Hamed Haghjo",
     author_email="hamedhaghjo@hotmail.com",
+    description="CLI tool to extract the code cells of a jupyter notebook into a .txt file.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/RealWorga/jupyter-code-extractor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
